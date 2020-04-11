e08879ff9da16164bf5783251b101cb47c1a48c0
Cedrick Tabares <cedrick.tabares@media-meter.com>
2020-04-11 21:30:04 +0800

    separated components

--- src/app.js
+++ src/app.js
@@ -1,27 +1,13 @@
 import Vue from 'vue'
-
-Vue.component('accordion', {
-    props:[
-        'fruit'
-    ],
-    template: `
-        <div class="card">
-            <div class="card-content">
-                <span class="card-title">{{ fruit.title }}</span>
-                <button class="btn" @click="toggle = ! toggle">Description</button>
-                <p v-if="toggle">{{ fruit.description }}</p>
-            </div>
-        </div>
-    `,
-    data(){
-        return {
-            toggle: false
-        }
-    }
-})
+import Accordion from './components/Accordion.vue'
 
 new Vue({
     el: '#app',
+
+    components:{
+        Accordion
+    },
+
     data:{
         items: [
             {title: 'Avocado', description:'Yum!'},
---
+++ src/components/Accordion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <span class="card-title">{{ fruit.title }}</span>
+            <button class="btn" @click="toggle = ! toggle">Description</button>
+            <p v-if="toggle">{{ fruit.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Accordion",
+    
+    props:[
+        'fruit'
+    ],
+
+    data(){
+        return {
+            toggle: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
